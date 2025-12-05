8cf5dc75e68aae585ae48c8c84536ffcc6702446
Felipe Gava <161661597+FelpaGava@users.noreply.github.com>
2025-12-05 11:59:01 -0300

    FIX - Correção em arquivos inativos

--- DDD.Infrastructure/Entities/Parametro.cs
+++
@@ -1,9 +0,0 @@
-namespace DDD.Infrastructure.Entities;
-
-public class Parametro
-{
-    public int PARID { get; set; }
-    public string PARNOMEPROJETO { get; set; } = string.Empty;
-    public string PARLINKLINKEDIN { get; set; } = string.Empty;
-    public string PARLINKGIT { get; set; } = string.Empty;
-}
